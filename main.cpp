0cb8865a6a1f40e6a798e85e3137db76450b0688
5-12 <hellowordare@163.com>
2025-04-25 12:22:19 +0800

    begin

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-#include <GLFW/glfw3.h> // 假设找到的是 GLFW 库
+#include <GLFW/glfw3.h>
+
 int main() {
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(640, 480, "114514", NULL, NULL);
+    if (!glfwInit()) return -1;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "改这里写标题", NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
+
+    glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* 你的代码写这里 */
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glfwTerminate();
+    return 0;
 }
